8755f2afe3392cfd7e41ef8635719f3ba3cef580
Lucas Ackman <AckmanLD@gmail.com>
2024-06-12 22:04:35 -0500

    Added Movable Paddles

--- Pong-Breaker/Assets/_Project/_Scripts/Collision Detection/Brick.cs
+++ Pong-Breaker/Assets/_Project/_Scripts/Collision Detection/Brick.cs
@@ -1,15 +1,17 @@
-using Pong_Breaker;
 using UnityEngine;
 
-public class Brick : MonoBehaviour, ICollidable
+namespace Pong_Breaker
 {
-    private void Start()
+    public class Brick : MonoBehaviour, ICollidable
     {
-        CollidableMediator.Register(this, transform.position);
-    }
+        private void Start()
+        {
+            CollidableMediator.Register(this, transform.position);
+        }
 
-    public void OnCollision(Ball ball)
-    {
-        Destroy(gameObject);
+        public void OnCollision(Ball ball)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Pong-Breaker/Assets/_Project/_Scripts/Paddle/Paddle.cs
@@ -0,0 +1,45 @@
+using Kickstarter.Inputs;
+using UnityEngine;
+
+namespace Pong_Breaker
+{
+    public class Paddle : MonoBehaviour, ICollidable, IInputReceiver
+    {
+        [SerializeField] private FloatInput movePaddle;
+        [SerializeField] private float movementSpeed = 1f; // [m/s
+
+        private float rawInput;
+
+        // Readonly / Components
+        private Rigidbody body;
+
+        #region InputHandler
+        public void RegisterInputs(Player.PlayerIdentifier playerIdentifier)
+        {
+            movePaddle.RegisterInput(OnMovePaddleInputChange, playerIdentifier);
+        }
+
+        public void DeregisterInputs(Player.PlayerIdentifier playerIdentifier)
+        {
+            movePaddle.DeregisterInput(OnMovePaddleInputChange, playerIdentifier);
+        }
+
+        private void OnMovePaddleInputChange(float input)
+        {
+            rawInput = input;
+        }
+        #endregion
+
+        #region UnityEvents
+        private void Awake()
+        {
+            body = transform.root.GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            body.AddForce(new Vector3(0, rawInput * movementSpeed, 0) - body.velocity, ForceMode.VelocityChange);
+        }
+        #endregion
+    }
+}
